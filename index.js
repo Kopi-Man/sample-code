6b43d15f17b34404df9168a5582382a9984882f2
Kopi-Man <52097700+Kopi-Man@users.noreply.github.com>
2020-03-10 16:33:22 +0530

    creating home

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+class home extends React.Components{
+ render(){
+   return (
+     <div>
+     <h1>Hello world</h1>
+     </div>
+   );
+ }
+}
+
 ReactDom.render(<Home>,document.getElementById('root'));
